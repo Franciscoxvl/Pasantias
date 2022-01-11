6af5b02c7048a18ad5d495a45e2b5cad3e56f1f4
Francisco <franc@DESKTOP-0BHPPMG.(none)>
2022-01-11 11:17:56 -0500

    Carga inicial del proyecto

--- src/Ejemplo.java
+++ src/Ejemplo.java
@@ -3,7 +3,11 @@ public class Ejemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] x = {1, 2 , 3, 4};
 		System.out.println("Hola mundo");
+		for (int i:x) {
+			System.out.println(i);
+		}
 
 	}
 
